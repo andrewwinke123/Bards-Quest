175f01d942f9aec574269aa0605ee1224169cc96
andrewwinke123 <andrewwinke@gmail.com>
2023-03-31 19:05:29 -0400

    making the input feild work with the enter key

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-test
+const input = document.getElementById("start");
+
+input.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    const userInput = input.value;
+    console.log(userInput);
+  }
+});
