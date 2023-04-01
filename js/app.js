f73319d4d0b85ed8cb34a2d67cf1b35727539b19
andrewwinke123 <andrewwinke@gmail.com>
2023-04-01 12:42:50 -0400

    organizing the start of the code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
 const input = document.getElementById("start");
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
 
 input.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
-    const userInput = input.value;
-    console.log(userInput);
+    const userInput = input.value
+    console.log(userInput)
   }
-});
+})
+
+/*-------------------------------- Functions --------------------------------*/
