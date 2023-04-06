cdd513c2568b696dbd2de0a0585947b08ee80f8b
andrewwinke123 <andrewwinke@gmail.com>
2023-04-05 20:54:02 -0400

    finishing up the story paths and branch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+
+const state = {
+  collectFly: false
+}
+
+
 let audio = new Audio("../sounds/typing-short.mp3")
 import {story} from "./data.js"
 
@@ -9,23 +15,23 @@ audio.play()
 const input = document.getElementById("start-input")
 const titleScreen = document.getElementById("title-screen")
 const buttons = document.querySelector("#button")
+const nameInput = document.getElementById("start-input")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 //control for input box for open of game, leading into the opening plot and the transition to the story
-const nameInput = document.getElementById("start-input")
 nameInput.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
-    const userInput = input.value
+      const userInput = input.value
+      input.value = ''
+      titleScreen.innerHTML = 
+      `<div class="title anim-typewriter"><h1>${story[story.chapters].title}</h1></div>
+      <h3>${story[story.chapters].story}</h3>
+      ${playerInput()}`
+      addInputListeners()
+    }
   }
-  
-  input.value = ''
-  titleScreen.innerHTML = 
-  `<h1>${story[story.chapters].title}</h1>
-  <h3>${story[story.chapters].story}</h3>
-  ${playerInput()}`
-  addInputListeners()
-})
+)
 
 
 
@@ -35,6 +41,10 @@ function addInputListeners() {
     inputButtons[i].addEventListener('click', handlePlayerInput)
   }
 }
+
+
+
+
 
 
 
@@ -50,44 +60,20 @@ function render() {
 // if (story[story.chapters].image) {
 //   image = '<img></img>'
 // }
-titleScreen.innerHTML = 
-`<h1>${story[story.chapters].title}</h1>
-<h3>${story[story.chapters].story}</h3>
-${image}
-${playerInput()}`
-addInputListeners() 
-// const img = document.createElement('img')
-// img.src=`url(./img/${story[story.chapters].image})`
-// img.classList.add('tower')
-// document.body.appendChild(img)
-bodyEl.style.backgroundImage = `url(./img/${story[story.chapters].image})`
+  titleScreen.innerHTML = 
+  `<div class="title anim-typewriter"<h1>${story[story.chapters].title}</h1></div>
+  <h3>${story[story.chapters].story}</h3>
+  ${image}
+  ${playerInput()}`
+  addInputListeners() 
+  // const img = document.createElement('img')
+  // img.src=`url(./img/${story[story.chapters].image})`
+  // img.classList.add('tower')
+  // document.body.appendChild(img)
+  bodyEl.style.backgroundImage = `url(./img/${story[story.chapters].image})`
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function inputValue() {
-// let inputs = document.querySelectorAll('input[type="button"]')
-// for (let i = 0; i < inputs.length; i++) {
-// if (inputs[i].clicked) {
-// story.chapters = (inputs[i].getAttribute('data-result'))
-// render()
-// return
-// }
-// }
-// story.chapters = story[story.chapters].defaultResult
-// render()
-// }
 
 
 
@@ -96,42 +82,35 @@ playerInput()
 function playerInput() {
 let input = ''
 if (!story[story.chapters].choices) {
-  return ''
+    return ''
 }
 for(let i = 0; i <  story[story.chapters].choices.length; i++) {
-input +=
-  `<div class="button" id="buttons" class="button-box">
-  <button data-result = ${story[story.chapters].choices[i].result} 
-  id="button">${story[story.chapters].choices[i].choice}</button>
-  </div>`
-}
-audio.play()
-return input
+    input +=
+      `<div class="button" id="buttons" class="button-box">
+      <button data-result = ${story[story.chapters].choices[i].result} 
+      id="button">${story[story.chapters].choices[i].choice}</button>
+      </div>`
+  }
+  audio.play()
+  return input
 
 }
-
-
-
-
-
-
 
 
 function handlePlayerInput(event) {
 const result = event.target.getAttribute('data-result')
 if (result) {
-story.chapters = result
-render()
+  story.chapters = result
+  render()
+  }
 }
+
+
+
+
+window.onload = function() {
+  const audio = new Audio("../sounds/typing-short.mp3");
+  audio.play();
 }
 
 
-
-
-
-
-
-// const restartButton = document.querySelector("#restartGame")
-// function restartGame(restartButton) {
-//   return titleScreen
-// }
--- js/data.js
+++ js/data.js
@@ -9,36 +9,35 @@ let story = {chapters:
 ]
 },
 beginQuest: {
-  title: 'Welcome to Gliks Tower', 
-  story:`<div class="flex-child-3"> The Wizard Glik has just stepped out to run errands. He will be back in 5 minutes.
+  title: 'The Wizard Glik has just stepped out to run errands.', 
+  story:`<div class="story-intro"> He will be back in 5 minutes.
   </div></p>
   <p>
-  "Don't forget your chores and you are NOT allowed in my crafting room! OR the observation deck" the wizard says.
+  "Don't forget your chores. And you are NOT allowed in my crafting room! OR the observation deck" the wizard says.
   </p>
-  <div class="flex-child-3"> <p>
+  <div class="story-intro"> <p>
   As the wizard leaves and you mosey around the his tower, you daydream about one day finding a way out of the wizards legally biding contract spell,
   which you foolishly fell for one month prior, and which legally bound you to his employment for the next 10 years. (Gamehindge is a "right to work state," and as such, workers rights are not very abundant). 
   For weeks you have been sneaking into his crafting room to learn his spells.
   You have hit a brick wall with your discrete training however, and the wizard doesn't leave for long thanks to his power of teleportaion. 
-  You look around the tower and notice that a fly has fallen onto the window ledge.
+  You look around the tower and notice flies gathering outside the window.
   "just the thing I need to start my first spell" you think to yourself. What do you do?</div>`,
 choices: [
-  {choice: 'Work on chores', result: 'wizardsTower'},
-  {choice: 'Enter crafting room', result: 'craftingRoom'},
-  {choice: 'Enter observation deck', result: 'craftingRoom'},
-  {choice: 'Collect fly', result: 'fly'},
+  {choice: 'Look around', result: 'wizardsTower'},
+  {choice: 'Enter crafting room', result: 'locked'},
+  {choice: 'Enter observation deck', result: 'observationDeck'},
   {choice: 'Enter Kitchen', result: 'kitchen'}
 ]
 },
 wizardsTower: {
-title: 'You mop around the tower and continue to daydream of life in the big city',
-story:`<div class="flex-child-4">As you do chores you think about your plan to escape. There are no doors to exit the tower. The Wizard Glik teleports whenever he leaves
-so you know there might not be a door at all. But perhaps there is one hidden around here somewhere...</div>`,
+title: 'You sweep and continue to dream of life in the big city',
+story:`As you do chores you think about your plan to escape. There are no doors to exit the tower. The Wizard Glik teleports whenever he leaves
+so you know there might not be a door at all. But perhaps there is one hidden around here somewhere...`,
 image: 'WizardsTower.jpg',
 choices: [
   {choice: 'Enter crafting room', result: 'locked'},
   {choice: 'Enter observation deck', result: 'observationDeck'},
-  {choice: 'Collect fly', result: 'fly'},
+  {choice: 'Enter Kitchen', result: 'kitchen'}
 ]
 },
 craftingRoom: {
@@ -46,28 +45,26 @@ title: `You enter the little office where Glik studies his texts. Across from yo
 story: 'Across from you, you see a table with multiple beakers of liquids, and a book which you recognize as his spell book.',
 image: '1.png',
 choices: [
-{ choice: 'Open book', result: 'openBook'},
   {choice: 'Mix potions', result: 'mixPotions'},
-  {choice: 'Continue Chores', result: 'Just as you begin..'}
+  {choice: 'Turn back', result: 'turnAround'}
 ]
 },
 fly: {
-  title: 'You pick up the fly',
-  story: 'Remembering that fly wings, or eagle feathers, are a crucial component of the fly spell, you save these for later.',
+  title: 'You swat at the flies',
+  story: 'As you swat the flies away, you remember that fly wings, or eagle feathers, are a crucial component of the fly spell, you squash one and save it for later.',
   choices: [
-    {choice: 'Work on chores', result: 'wizardsTower'},
-    {choice: 'Enter crafting room', result: 'craftingRoom'},
-    {choice: 'Enter observation deck', result: 'observation deck'},
+    {choice: 'Blue liquid', result: 'blue'},
+    {choice: 'Red liquid', result: 'red'},
+    {choice: 'Turn back', result: 'turnAround'}
   ]
 },
 locked: {
   title: 'The door is locked tight',
-  story:'The Wizard Glik enters the room and...',
+  story:`The Wizard Glik doesn't want anyone going in here`,
   choices: [
-    {choice: 'Enter crafting room', result: 'Just as you begin..'},
-    {choice: 'Enter observation deck', result: 'Just as you begin..'},
-    {choice: 'Collect fly', result: 'Just as you begin..'},
-    {choice: 'Continue Chores', result: 'Just as you begin..'}
+    {choice: 'Enter observation deck', result: 'observationDeck'},
+    {choice: 'Continue Chores', result: 'Just as you begin..'},
+    {choice: 'Enter Kitchen', result: 'kitchen'}
    ]
   },
   kitchen: {
@@ -77,97 +74,224 @@ locked: {
       {choice: 'Blue liquid', result: 'blue'},
       {choice: 'Red liquid', result: 'red'},
       {choice: 'Check the fridge', result: 'fridge'},
-    {choice: 'Work on chores', result: 'justAsYouBegin'}
+      {choice: 'Turn back', result: 'turnAround'}
     ]
   },
-  mixPotions: {
-    title: 'You mix the potions',
-    story:'The Wizard Glik enters the room and...',
+  fridge: {
+    title: 'You open the fidge...',
+    story:'Completely empty.... damn. nothing but flies',
     choices: [
-      {choice: 'Enter crafting room', result: 'Just as you begin..'},
-      {choice: 'Enter observation deck', result: 'Just as you begin..'},
-      {choice: 'Collec tfly', result: 'Just as you begin..'},
-      {choice: 'Continue Chores', result: 'Just as you begin..'}
+      {choice: 'Blue liquid', result: 'blue'},
+      {choice: 'Red liquid', result: 'red'},
+      {choice: 'Swat at flies', result: 'fly'},
+      {choice: 'Turn back', result: 'turnAround'}
+
+    ]
+  },
+  turnAround: {
+    title: 'You turn around and head back to the main room',
+    story:'As you enter the main room of the tower you contemplate your next move.',
+    choices: [
+      {choice: 'Look around', result: 'wizardsTower'},
+      {choice: 'Enter crafting room', result: 'craftingRoom'},
+      {choice: 'Enter observation deck', result: 'observationDeck'},
+      {choice: 'Enter Kitchen', result: 'kitchen'}
     ]
   },
   observationDeck: {
-    title: 'Just as you begin..',
-    story:'The Wizard Glik enters the room and...',
+    title: 'You step outside and breath in the air',
+    story:`"FRESH AIR" you say. isn't it nice? Other than the swarm of flies that is pestering you as you stretch. From the observation deck you can see the town of Schmof, 
+    a small village which supplies Prussia with grain. to the North you see prussia. As you look around the observation deck you see Gliks telescope, a lawn chair, and a table full of empty beer.`,
     choices: [
-    {choice: 'Enter crafting room', result: 'Just as you begin..'},
-    {choice: 'Enter observation deck', result: 'Just as you begin..'},
-    {choice: 'Collec tfly', result: 'Just as you begin..'},
-    {choice: 'Continue Chores', result: 'Just as you begin..'}
+    {choice: 'Look in telescope', result: 'telescope'},
+    {choice: 'Take a seat', result: 'seat'},
+    {choice: 'Search table', result: 'table'},
+    {choice: 'Head back inside', result: 'turnAround'}
   ]
-  },
-  blue: {
-    title: 'Just as you begin..',
-    story:'The Wizard Glik enters the room and...',
+},
+  telescope: {
+    title: 'You look into Gliks telescope',
+    story:'You look up, you look down, you look all around. The north you can just barely see travelers reaching the gates of prussia. To the south can see all of Schmof and everyone in it. You see the law offices of Celebrimbor, prominent workplace lawyer; perhaps you could use his help later.',
     choices: [
-      {choice: 'Enter crafting room', result: 'Just as you begin..'},
-      {choice: 'Enter observation deck', result: 'Just as you begin..'},
-      {choice: 'Collec tfly', result: 'Just as you begin..'},
-      {choice: 'Continue Chores', result: 'Just as you begin..'}
-    ]
-  },
-  red: {
-    title: 'Just as you begin..',
-      story:'The Wizard Glik enters the room and...',
-      choices: [
-      {choice: 'Enter crafting room', result: 'Just as you begin..'},
-      {choice: 'Enter observation deck', result: 'Just as you begin..'},
-      {choice: 'Collec tfly', result: 'Just as you begin..'},
-      {choice: 'Continue Chores', result: 'Just as you begin..'}
-    ]
-  },
-  justAsYouBegin: {
-      title: 'Just as you begin..',
-      story:'The Wizard Glik enters the room and...',
-      choices: [
-        {choice: 'Enter crafting room', result: 'craftingRoom'},
-        {choice: 'Enter observation deck', result: 'Just as you begin..'},
-        {choice: 'Collec tfly', result: 'Just as you begin..'},
-        {choice: 'Continue Chores', result: 'Just as you begin..'}
+      {choice: 'Take a seat', result: 'seat'},
+      {choice: 'Search table', result: 'table'},
+      {choice: 'Head back inside', result: 'turnAround'}
       ]
-  },
-  justAsYouBegin: {
-    title: 'Just as you begin..',
-    story:'The Wizard Glik enters the room and...',
+    },
+  table: {
+    title: 'You check all around the table',
+    story:'Nothing but empties... and Gliks journal.',
     choices: [
-      {choice: 'Enter crafting room', result: 'gliksHome'},
-      {choice: 'Enter observation deck', result: 'gliksHome'},
-      {choice: 'Collect fly', result: 'gliksHome'},
-      {choice: 'Continue Chores', result: 'glikLeaves'}
+      {choice: 'Take a seat', result: 'seat'},
+      {choice: 'Look in telescope', result: 'telescope'},
+      {choice: 'Head back inside', result: 'turnAround'},
+      {choice: 'Open book', result: 'openBook'},
+    ]
+  },
+  seat: {
+    title: 'You sit down....',
+    story:'And the chair is so comfortable to fall asleep...',
+    choices: [
+      {choice: 'sleep', result: 'gliksHome'},
     ]
   },
   gliksHome: {
     title: 'ZaaaAAAaaaAAAaap!',
-    story:`"jUST WHAT DO YOU THINK YOU ARE DOING IN A RESTRICTED SPACE?" Glik yells`,
+    story:`"jUST WHAT DO YOU THINK YOU ARE DOING IN A RESTRICTED SPACE?" Glik yells
+    <div class="lose"><p>YOU LOSE!!!!!!</P></div>`,
     choices: [
-      {choice: 'Enter crafting room', result: 'gliksHome'},
-      {choice: 'Enter observation deck', result: 'gliksHome'},
-      {choice: 'Continue Chores', result: 'glikLeaves'}
+      {choice: 'Restart Game', result: 'chapter1'}
     ]
   },
-glikLeaves: {
-  title: 'After an indeterminate amount of time doing chores....',
-  story:'Glik finally heads for town again. "cant wait until im in Prussia playing music on the big stage!" you think to yourself.',
+blue: {
+  title: 'You drink the blue potion..',
+  story:`You are now a bluejay. Not quite what you wanted, but now you can live your days flying the skies which is pretty cool too I guess
+  <div class="lose"><p>YOU LOSE?<p></div>`,
   choices: [
-    {choice: 'Work on chores', result: 'wizardsTower'},
-    {choice: 'Enter crafting room', result: 'craftingRoom'},
-    {choice: 'Enter observation deck', result: 'observation deck'},
-    {choice: 'Collect fly', result: 'fly'}
+    {choice: 'Restart Game', result: 'chapter1'}
   ]
 },
-  openBook: {
-  title: 'Just as you begin..',
-  story:`The Wizard Glik enters the room and...`,
+redF: {
+  title: 'You drink the red potion...',
+    story:'Right as you finish it off you begin to shrink. Understanding that this is likely temporary you rush to to Gliks crafting room where you slip under the door',
+    choices: [
+    {choice: 'Look around crafting room', result: 'craftingRoom'},
+  ]
+},
+openBook: {
+  title: 'You open Gliks journal...',
+  story:`The book marked page goes into detail about how fly and eagle wings can be used to craft potions of flight.`,
   image: '2.png',
   choices: [
-  {choice: 'Restart Game', result: 'chapter1'}
+    {choice: 'Take a seat', result: 'seat'},
+    {choice: 'Look in telescope', result: 'telescope'},
+    {choice: 'Head back inside', result: 'turnAround'},
   ]
-  }
-  
+},
+mixPotions: {
+  title: 'You mix the potions',
+  story:'The Wizard Glik enters the room and...',
+  choices: [
+    {choice: 'Fly to Schmof', result: 'schmof'},
+    {choice: 'Fly to Prussia', result: 'prussia'},
+  ]
+},
+prussia: {
+  title: 'Just as you begin..',
+  story:'The Wizard Glik enters the room and...',
+  choices: [
+    {choice: 'Restart Game', result: 'chapter1'}
+  ]
+},
+schmof: {
+  title: 'Just as you begin..',
+  story:'The Wizard Glik enters the room and...',
+  choices: [
+    {choice: 'Restart Game', result: 'chapter1'}
+  ]
+},
+
+/////////////////////////////////////////////////////////
+
+kitchenF: {
+  title: 'You enter the kitchen',
+  story:'The wizard rarely eats but the kitchen is still a mess. empty beakers everywhere on the counter, and 3 full ones in the midst of the broken glass.',
+  choices: [
+    {choice: 'Blue liquid', result: 'blue'},
+    {choice: 'Red liquid', result: 'redF'},
+    {choice: 'Check the fridge', result: 'fridgeF'},
+    {choice: 'Turn back', result: 'turnAroundF'}
+  ]
+},
+fridgeF: {
+  title: 'You open the fidge...',
+  story:'Completely empty.... damn. nothing but flies',
+  choices: [
+    {choice: 'Blue liquid', result: 'blue'},
+    {choice: 'Red liquid', result: 'redF'},
+    {choice: 'Turn back', result: 'turnAroundF'}
+
+  ]
+},
+turnAroundF: {
+  title: 'You turn around and head back to the main room',
+  story:'As you enter the main room of the tower you contemplate your next move.',
+  choices: [
+    {choice: 'Look around', result: 'wizardsTowerF'},
+    {choice: 'Enter crafting room', result: 'craftingRoomF'},
+    {choice: 'Enter observation deck', result: 'observationDeckF'},
+    {choice: 'Enter Kitchen', result: 'kitchenF'}
+  ]
+},
+observationDeckF: {
+  title: 'You step outside and breath in the air',
+  story:`"FRESH AIR" you say. isn't it nice? Other than the swarm of flies that is pestering you as you stretch. From the observation deck you can see the town of Schmof, 
+  a small village which supplies Prussia with grain. to the North you see prussia. As you look around the observation deck you see Gliks telescope, a lawn chair, and a table full of empty beer.`,
+  choices: [
+  {choice: 'Look in telescope', result: 'telescopeF'},
+  {choice: 'Take a seat', result: 'seatF'},
+  {choice: 'Search table', result: 'tableF'},
+  {choice: 'Head back inside', result: 'turnAroundF'}
+]
+},
+telescopeF: {
+  title: 'You look into Gliks telescope',
+  story:'You look up, you look down, you look all around. The north you can just barely see travelers reaching the gates of prussia. To the south can see all of Schmof and everyone in it. You see the law offices of Celebrimbor, prominent workplace lawyer; perhaps you could use his help later.',
+  choices: [
+    {choice: 'Take a seat', result: 'seatF'},
+    {choice: 'Search table', result: 'tableF'},
+    {choice: 'Head back inside', result: 'turnAroundF'}
+    ]
+  },
+tableF: {
+  title: 'You check all around the table',
+  story:'Nothing but empties... and Gliks journal.',
+  choices: [
+    {choice: 'Take a seat', result: 'seatF'},
+    {choice: 'Look in telescope', result: 'telescopeF'},
+    {choice: 'Head back inside', result: 'turnAroundF'},
+    {choice: 'Open book', result: 'openBookF'},
+  ]
+},
+openBookF: {
+  title: 'You open Gliks journal...',
+  story:`The book marked page goes into detail about how fly and eagle wings can be used to craft potions of flight.`,
+  image: '2.png',
+  choices: [
+    {choice: 'Take a seat', result: 'seatF'},
+    {choice: 'Look in telescope', result: 'telescopeF'},
+    {choice: 'Head back inside', result: 'turnAroundF'},
+  ]
+},
+wizardsTowerF: {
+  title: 'You sweep and continue to dream of life in the big city',
+  story:`As you do chores you think about your plan to escape. There are no doors to exit the tower. The Wizard Glik teleports whenever he leaves
+  so you know there might not be a door at all. But perhaps there is one hidden around here somewhere...`,
+  image: 'WizardsTower.jpg',
+  choices: [
+    {choice: 'Enter crafting room', result: 'lockedF'},
+    {choice: 'Enter observation deck', result: 'observationDeckF'},
+    {choice: 'Enter Kitchen', result: 'kitchenF'}
+  ]
+  },
+  fly: {
+    title: 'You swat at the flies',
+    story: 'As you swat the flies away, you remember that fly wings, or eagle feathers, are a crucial component of the fly spell, you squash one and save it for later.',
+    choices: [
+      {choice: 'Blue liquid', result: 'blue'},
+      {choice: 'Red liquid', result: 'red'},
+      {choice: 'Turn back', result: 'turnAroundF'}
+    ]
+  },
+  lockedF: {
+    title: 'The door is locked tight',
+    story:'The Wizard Glik enters the room and...',
+    choices: [
+      {choice: 'Enter observation deck', result: 'observationDeckF'},
+      {choice: 'Continue Chores', result: 'Just as you begin..F'},
+      {choice: 'Enter Kitchen', result: 'kitchenF'}
+     ]
+    },
 }
   
 
